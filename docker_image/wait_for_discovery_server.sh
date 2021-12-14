06e9654ed0077c53e4b67ff0e83e1c7a742c93a5
dominikn <dominik.nwk@gmail.com>
2021-12-14 14:47:28 +0100

    testing ga

--- docker_image/wait_for_discovery_server.sh
+++ docker_image/wait_for_discovery_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Waiting for ddsdiscoveryserver device to be available in /etc/hosts"
+
 while [[ $(grep 'ddsdiscoveryserver' /etc/hosts | wc -l) -eq 0 ]]; do 
     echo "."
     sleep 1
 done
 
-echo "DDS Discovery Server host is available (in /etc/hosts)"
+echo "ddsdiscoveryserver present in /etc/hosts"

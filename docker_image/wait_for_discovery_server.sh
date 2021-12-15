37e41bbe379a49944643c7f2c6987fe9692f0cd2
dominikn <dominik.nwk@gmail.com>
2021-12-15 01:25:39 +0100

    nicer hostname for DDS Discovery Server

--- docker_image/wait_for_discovery_server.sh
+++ docker_image/wait_for_discovery_server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Waiting for \"ddsdiscoveryserver\" host to be available in /etc/hosts"
+echo "Waiting for \"dds-discovery-server\" host to be available in /etc/hosts"
 
-while [[ $(grep 'ddsdiscoveryserver' /etc/hosts | wc -l) -eq 0 ]]; do 
+while [[ $(grep 'dds-discovery-server' /etc/hosts | wc -l) -eq 0 ]]; do 
     sleep 1
 done
 
-echo "\"ddsdiscoveryserver\" present in /etc/hosts:"
+echo "\"dds-discovery-server\" present in /etc/hosts:"
 
 # print the IPv6 address of the Discovery Server
-grep 'ddsdiscoveryserver' /etc/hosts
+grep 'dds-discovery-server' /etc/hosts
 
 echo "Ready to launch ROS 2 nodes"
 
